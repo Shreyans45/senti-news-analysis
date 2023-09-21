89d7bd6250cd9e9f0278197bb77d86d5eeb8269d
Shreyans45 <94587906+Shreyans45@users.noreply.github.com>
2023-09-21 21:57:34 +0530

    Update Stopwords Removal.py

--- Stopwords Removal.py
+++ Stopwords Removal.py
@@ -1,1 +1,10 @@
+from nltk.corpus import stopwords
+import nltk
+nltk.download('stopwords')
+
+def remove_stopwords(text):
+    stop_words = set(stopwords.words('english'))
+    tokens = text.split()
+    filtered_tokens = [word for word in tokens if word.lower() not in stop_words]
+    return ' '.join(filtered_tokens)
 
