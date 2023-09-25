027fb17a897b98c7db1882a2f01e922e3943e69a
Shreyans45 <94587906+Shreyans45@users.noreply.github.com>
2023-09-25 21:13:23 +0530

    Create Classification.py

---
+++ Classification.py
@@ -0,0 +1,7 @@
+def classify_sentiment(aggregate_score):
+    if aggregate_score > 0:
+        return 'positive'
+    elif aggregate_score < 0:
+        return 'negative'
+    else:
+        return 'neutral'
